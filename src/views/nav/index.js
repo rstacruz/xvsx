5b3337f347e0da04fb4478e9d73d240952daf48c
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-11 21:23:52 +0800

    Implement a compact button

--- src/views/nav/index.js
+++ src/views/nav/index.js
@@ -1,3 +1,6 @@
+var removeClass = require('dom101/remove-class');
+var addClass = require('dom101/add-class');
+
 module.exports = Ractive.extend({
   template: require('./template.html'),
 
@@ -6,20 +9,34 @@ module.exports = Ractive.extend({
   },
 
   data: {
-    dropdownVisible: false
+    dropdownVisible: false,
+    compact: false
   },
 
   toggleDropdown: function () {
-    var shown = this.get('dropdownVisible');
-    shown = ! shown;
-    this.set('dropdownVisible', shown);
+    this.set('dropdownVisible', ! this.get('dropdownVisible'));
+  },
+
+  toggleCompact: function () {
+    this.set('compact', ! this.get('compact'));
+  },
+
+  observers: {
+    compact: function (val) {
+      if (val) addClass(document.body, '-layout-compact');
+      else removeClass(document.body, '-layout-compact');
+    }
   },
 
   computed: {
     titleize: function () {
       return function (lang) {
         return this.data.data.languages[lang].title;
-      }
+      };
     }
+  },
+
+  oninit: function () {
+    this.observe(this.observers);
   }
 });
