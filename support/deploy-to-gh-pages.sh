d3aeb0e30935cb4669869c64233bc96aed38eeba
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-16 14:13:57 +0800

    Travis: fix git failure

--- support/deploy-to-gh-pages.sh
+++ support/deploy-to-gh-pages.sh
@@ -8,9 +8,13 @@ mkdir public
 # build
 make
 
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
 # deploy
 cd public
 git init
 git add .
-git commit -m "Deploy to Github Pages" --author "Travis CI <nobody@nobody.org>"
+git commit -m "Deploy to Github Pages"
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 2>&1
