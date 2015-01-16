48434ee66cd2ebc0546378f18baa60c5ef20d440
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-16 14:04:02 +0800

    Tests: disable make test in CI

--- test/make_test.js
+++ test/make_test.js
@@ -4,7 +4,8 @@ var outlinify = require('../lib/bundle').outlinify;
 var exec = require('exec');
 
 describe('make', function () {
-  it('building data works', function (done) {
+  (process.env.CI ? xit : it)
+  ('building data works', function (done) {
     exec('make -B data', function(err, out, code) {
       if (code !== 0 || err !== '') {
         console.log(err);
